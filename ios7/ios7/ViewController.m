22155f1aaf2bb791072d47a4a32df0891413b597
chenzongyi <chenzongyi@163.com>
2015-12-15 18:22:21 +0800

    哈哈

--- ios7/ios7/ViewController.m
+++ ios7/ios7/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"哈哈");
+    NSLog(@"只提交到本地，没有提交到远程服务器");
 }
 
 - (void)didReceiveMemoryWarning {
