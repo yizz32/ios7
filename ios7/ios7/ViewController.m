c25adbd14b551132600c9b5986297d52e790463d
chenzongyi <chenzongyi@163.com>
2015-12-15 18:23:41 +0800

    你好

--- ios7/ios7/ViewController.m
+++ ios7/ios7/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"哈哈");
     NSLog(@"只提交到本地，没有提交到远程服务器");
+    NSLog(@"你好啊");
 }
 
 - (void)didReceiveMemoryWarning {
